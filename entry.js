3508d4b7176bb6b53c715f744267902b5d4d98d4
Kent <kcponline@outlook.com>
2016-12-08 21:23:44 -0800

    test remove bundle.js

--- entry.js
+++ entry.js
@@ -1,5 +1,6 @@
 // initial entry.js bundling test
 // document.write("It works. The edit bundling is working");
 
-// extra file content.js routing test
+// extra file content.js routing test of modules. only this file
+// is executed and requires all other modules
 document.write(require("./content.js"));
