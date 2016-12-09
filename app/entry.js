7ded02f88ca9681359d93b8369ecec70706c0db4
Kent <kcponline@outlook.com>
2016-12-09 00:16:02 -0800

    install css-loader style-loader and add style.css

--- app/entry.js
+++ app/entry.js
@@ -6,4 +6,5 @@
 // the path remains the same even when moved together
 // with the main entry.js file
 // the cli has to be webpack ./app/entry.js bundle.js
+require("!style!css!../assets/css/style.css");
 document.write(require("./content.js"));
