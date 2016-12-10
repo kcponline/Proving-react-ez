67caf9780e672c4419f92f751a41ccabb8fef67f
Kent <kcponline@outlook.com>
2016-12-10 13:03:18 -0800

    move react dom rendering from entry.js to content.js for modularization

--- app/content.js
+++ app/content.js
@@ -1,1 +1,25 @@
-module.exports = "It works from content.js.";
+
+	// example 2 - react modular extra file
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+module.exports = ReactDOM.render(
+	<div className='panel panel-default'>
+		<p>Hello Kent, I can edit this on the fly with react !</p>
+		<ol>
+			<li>run npm install or npm install webpack -g</li>
+			<li>run webpack -w</li>
+			<li>open the index.html from public folder in the browser</li>
+			<li>for every edit saved, webpack will automatically update to bundle.js</li>
+			<li>webpack's update will be logged in the console or terminal</li>
+			<li>refresh the webpage to visualize the updates</li>
+			<li>for multiple elements, put them in div and must end with comma</li>
+			<li>remember to exit webpack with ctrl c when finished</li>
+			<li>test1</li>
+		</ol>
+	</div>,
+	document.getElementById('app')
+)
+
+	// example 1 - extra file
+// module.exports = "It works from content.js.";
--- app/entry.js
+++ app/entry.js
@@ -1,24 +1,28 @@
-	// example 6 - simple boot strap className
-var React = require('react');
-var ReactDOM = require('react-dom');
+	// example 7 - react rendering modular extra file
+var contents = require("./content.js");
+module.exports = contents;
 
-ReactDOM.render(
-	<div className='panel panel-default'>
-		<p>Hello Kent, I can edit this on the fly with react !</p>
-		<ol>
-			<li>run npm install or npm install webpack -g</li>
-			<li>run webpack -w</li>
-			<li>open the index.html from public folder in the browser</li>
-			<li>for every edit saved, webpack will automatically update to bundle.js</li>
-			<li>webpack's update will be logged in the console or terminal</li>
-			<li>refresh the webpage to visualize the updates</li>
-			<li>for multiple elements, put them in div and must end with comma</li>
-			<li>remember to exit webpack with ctrl c when finished</li>
-			<li>test1</li>
-		</ol>
-	</div>,
-	document.getElementById('app')
-)
+	// example 6 - simple boot strap className
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+// ReactDOM.render(
+// 	<div className='panel panel-default'>
+// 		<p>Hello Kent, I can edit this on the fly with react !</p>
+// 		<ol>
+// 			<li>run npm install or npm install webpack -g</li>
+// 			<li>run webpack -w</li>
+// 			<li>open the index.html from public folder in the browser</li>
+// 			<li>for every edit saved, webpack will automatically update to bundle.js</li>
+// 			<li>webpack's update will be logged in the console or terminal</li>
+// 			<li>refresh the webpage to visualize the updates</li>
+// 			<li>for multiple elements, put them in div and must end with comma</li>
+// 			<li>remember to exit webpack with ctrl c when finished</li>
+// 			<li>test1</li>
+// 		</ol>
+// 	</div>,
+// 	document.getElementById('app')
+// )
 
 	// example 5 of react setup
 	// This code renders a simple "Hello Kent...etc".
